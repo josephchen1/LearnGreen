205c145fc737221459a0afa602a14cb0f1610649
Joseph <joseph@Josephs-Mac-mini.local>
2021-04-25 18:46:52 -0400

    Create landing page and add assets

---
+++ lib/MainPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: mainPage(),
+    );
+  }
+}
+class mainPage extends StatefulWidget {
+  @override
+  _mainPageState createState() => _mainPageState();
+}
+
+class _mainPageState extends State<mainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
